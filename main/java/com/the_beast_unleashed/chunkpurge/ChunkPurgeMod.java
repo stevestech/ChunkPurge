75f8958ce821d58d9135e9c8c5eeb20a3c5a4503
stevestech <francisbaster@gmail.com>
2014-09-15 02:51:41 +1200

    Fix indentation

--- main/java/com/the_beast_unleashed/chunkpurge/ChunkPurgeMod.java
+++ main/java/com/the_beast_unleashed/chunkpurge/ChunkPurgeMod.java
@@ -16,35 +16,35 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = ChunkPurgeMod.MODID, name = ChunkPurgeMod.NAME, version = ChunkPurgeMod.VERSION)
 public class ChunkPurgeMod
 {
-	
-    public static final String MODID = "ChunkPurge";
-    public static final String NAME = "Chunk Purge";
-    public static final String VERSION = "1.1";
-    
-    @Instance(MODID)
-    public static ChunkPurgeMod instance;
-    
-    public static Logger log;
-    
-    @SidedProxy(clientSide = "com.the_beast_unleashed.chunkpurge.proxy.ProxyClient", serverSide = "com.the_beast_unleashed.chunkpurge.proxy.ProxyCommon")
-    public static ProxyCommon proxy;
-    
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-    	proxy.preInit();
-    }
-    
-    @EventHandler
-    public void load(FMLInitializationEvent event)
-    {
+
+	public static final String MODID = "ChunkPurge";
+	public static final String NAME = "Chunk Purge";
+	public static final String VERSION = "1.1";
+
+	@Instance(MODID)
+	public static ChunkPurgeMod instance;
+
+	public static Logger log;
+
+	@SidedProxy(clientSide = "com.the_beast_unleashed.chunkpurge.proxy.ProxyClient", serverSide = "com.the_beast_unleashed.chunkpurge.proxy.ProxyCommon")
+	public static ProxyCommon proxy;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		proxy.preInit();
+	}
+
+	@EventHandler
+	public void load(FMLInitializationEvent event)
+	{
 		proxy.load();
-    }
-    
-    @EventHandler
-    public void postInit(FMLPostInitializationEvent event)
-    {
-    	proxy.postInit();
-    }
-    
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		proxy.postInit();
+	}
+
 }
